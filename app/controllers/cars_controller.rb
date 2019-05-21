186a171b44ceae0043b40a966b949d127ea87419
Jackson Huang <jacksonh201325@gmail.com>
2019-05-21 16:00:45 +0200

    Added mycars, new car as owner" "

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,17 +1,28 @@
 class CarsController < ApplicationController
   def index
+
+  end
+
+  def mycars
+    @cars = current_user.cars
+  end
+  
+  def new
+    @car = Car.new
   end
 
   def create
-  end
-
-  def new
+    @car = Car.new(car_params)
+    @car.user = current_user
+    @car.save
+    redirect_to
   end
 
   def edit
   end
 
   def show
+    @car = Car.find(params[:id])
   end
 
   def update
@@ -20,4 +31,9 @@ class CarsController < ApplicationController
   def destroy
   end
 
+  private
+
+  def car_params
+    params.require('car').permit(:address, :user_id, :year, :brand, :model, :odometer, :transmission, :license_plate, :description, :price_per_day)
+  end
 end
