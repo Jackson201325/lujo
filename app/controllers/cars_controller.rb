2f972de7ffd4f34301fd8d6993a656bacad6481a
Jackson Huang <jacksonh201325@gmail.com>
2019-05-22 11:04:31 +0200

    added back links to cards

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,4 +1,5 @@
 class CarsController < ApplicationController
+  skip_before_action :authenticate_user!, only:[:index, :show]
   def index
     @cars = Car.all
     policy_scope(@cars)
@@ -7,6 +8,8 @@ class CarsController < ApplicationController
 
   def my_cars
     @cars = current_user.cars
+    session[:return_to] ||= request.referer
+
     authorize(@cars)
   end
 
@@ -29,6 +32,7 @@ class CarsController < ApplicationController
   def show
     @car = Car.find(params[:id])
     authorize(@car)
+    # raise
   end
 
   def update
