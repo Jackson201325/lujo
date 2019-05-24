bac8465908a45a0722f17387184e13d045cc265a
Lukas Bollinger <lukas.bollinger21@gmail.com>
2019-05-24 15:14:23 +0200

    Fixed only one AJAX request issue w function

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,14 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["unique", "bold", "outstanding", "interesting"],
-    typeSpeed: 50,
-    loop: false,
-  });
+  const bannerText = document.getElementById("#banner-typed-text");
+  if (bannerText) {
+    new Typed('#banner-typed-text', {
+      strings: ["unique", "bold", "outstanding", "interesting"],
+      typeSpeed: 50,
+      loop: false,
+    });
+  }
 }
 
 export { loadDynamicBannerText };
